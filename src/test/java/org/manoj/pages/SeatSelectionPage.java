5197004c42f0534f762525a900f3b3246f83df24
manojkumarthakur <manoj.test.acc@gmail.com>
2025-08-28 09:13:45 +0530

    Found ticket pricing

--- src/test/java/org/manoj/pages/SeatSelectionPage.java
+++ src/test/java/org/manoj/pages/SeatSelectionPage.java
@@ -1,10 +1,54 @@
 package org.manoj.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class SeatSelectionPage extends BasePage{
 
+    @FindBy(xpath = "//div[@data-autoid='seatContainer']//*[contains(@aria-label,'seat status available')]")
+    List<WebElement> availableSeats;
+
     public SeatSelectionPage(WebDriver driver) {
         super(driver);
+    }
+
+    public int availableSeatsCount() {
+        wait.until(ExpectedConditions.visibilityOfAllElements(availableSeats));
+        return availableSeats.size();
+    }
+
+    public int[] minMaxSeatCharges() {
+        int min =0;
+        int max =0;
+        int[] minMax = new int[2];
+        int count = 0;
+
+        for (int i=0; i<availableSeats.size(); i++) {
+            WebElement element = availableSeats.get(i);
+            String priceWithSymbol = element.getText();
+            System.out.println(priceWithSymbol);
+            String priceStr = priceWithSymbol.substring(1);
+            int price = Integer.valueOf(priceStr);
+            if (count == 0) {
+                min = price;
+                max = price;
+            }
+            if (price < min) {
+                min = price;
+            }
+            if (price > max) {
+                max = price;
+            }
+            count++;
+        }
+
+        minMax[0] = min;
+        minMax[1] = max;
+        return minMax;
     }
 }
--- src/test/java/org/manoj/tests/RedBusCheckBookingTest.java
+++ src/test/java/org/manoj/tests/RedBusCheckBookingTest.java
@@ -3,18 +3,23 @@ package org.manoj.tests;
 import org.manoj.Utils.TestUtils;
 import org.manoj.pages.RedBusHomePage;
 import org.manoj.pages.SearchResultPage;
+import org.manoj.pages.SeatSelectionPage;
 import org.testng.annotations.Test;
 
 public class RedBusCheckBookingTest extends BaseTest{
 
     public final String redBusURL = "https://www.redbus.in/";
-    private RedBusHomePage homePage;
-    private SearchResultPage resultPage;
     public static String from = "Bangalore";
     public static String to = "Chennai";
 
+    private RedBusHomePage homePage;
+    private SearchResultPage resultPage;
+    private SeatSelectionPage seatSelectionPage;
 
-    @Test
+
+
+
+    @Test(priority = 0)
     public void navigateToRedBus() {
         driver.get(redBusURL);
         homePage = new RedBusHomePage(driver);
@@ -26,14 +31,27 @@ public class RedBusCheckBookingTest extends BaseTest{
 //        TestUtils.sleep(2000);
         homePage.selectDate(1);
         homePage.clickSearch();
-
     }
 
-    @Test
+    @Test(priority = 1)
     public void selectBus() {
         resultPage = new SearchResultPage(driver);
         TestUtils.sleep(2000);
         resultPage.selectBus(2);
-        TestUtils.sleep(5000);
+        TestUtils.sleep(2000);
     }
+
+
+    @Test(priority = 2)
+    public void checkAvailibility() {
+        seatSelectionPage = new SeatSelectionPage(driver);
+        TestUtils.sleep(2000);
+        System.out.println("Total available seats are: " + seatSelectionPage.availableSeatsCount());
+        TestUtils.sleep(2000);
+        int[] minMaxPrice = seatSelectionPage.minMaxSeatCharges();
+        System.out.println("Minimum ticket price is : " + minMaxPrice[0]);
+        System.out.println("Maximum ticket price is : " + minMaxPrice[1]);
+    }
+
+
 }
